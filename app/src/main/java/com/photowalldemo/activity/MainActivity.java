a2cc9f96c88cc013c66cafcfab72846ded5bacb9
FlyStorm <ggloading@gmail.com>
2017-11-18 14:11:45 +0800

    Initial commit

---
+++ app/src/main/java/com/photowalldemo/activity/MainActivity.java
@@ -0,0 +1,37 @@
+package com.photowalldemo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+
+import com.photowalldemo.adapter.PhotoWallAdapter;
+import com.photowalldemo.entity.Images;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * 用于展示照片墙的GridView
+     */
+    private GridView mPhotoWall;
+
+    /**
+     * GridView的适配器
+     */
+    private PhotoWallAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPhotoWall = (GridView) findViewById(R.id.photo_wall);
+        adapter = new PhotoWallAdapter(this, 0, Images.imageThumbUrls, mPhotoWall);
+        mPhotoWall.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 退出程序时结束所有的下载任务
+        adapter.cancelAllTasks();
+    }
+}
